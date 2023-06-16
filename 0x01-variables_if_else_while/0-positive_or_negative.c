14586ab2cb5c67095ca81a1eb7b6209cfcfce4d7
ayoubidb <ayoubidb@gmail.com>
2023-06-16 14:30:23 -0700

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,9 +7,9 @@
  * negative, or zero. It uses the srand and rand functions from the standard
  * library, along with the time function to seed the random number generator.
  * The generated random number is stored in the variable n. The program then
- * prints the value of n and determines its sign, providing an appropriate
- * message. The output includes the number, followed by whether it is positive,
- * negative, or zero, each on a new line.
+ * determines the sign of the number and provides an appropriate message.
+ * The output includes whether the number is positive, negative, or zero,
+ * each on a new line.
  */
 
 int main(void)
@@ -19,16 +19,14 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2;
 
-    printf("The number is: %d\n", n);
-
     if (n > 0) {
-        printf("%d is positive.\n", n);
+        printf("%d is positive\n", n);
     }
     else if (n < 0) {
-        printf("%d is negative.\n", n);
+        printf("%d is negative\n", n);
     }
     else {
-        printf("%d is zero.\n", n);
+        printf("%d is zero\n", n);
     }
 
     return 0;
