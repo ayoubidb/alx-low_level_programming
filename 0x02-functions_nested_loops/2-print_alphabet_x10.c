a3be7d05be9d13915b59e04795434f127ca1dbde
ayoubidb <ayoubidb@gmail.com>
2023-06-20 14:42:37 -0700

    My first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- *prints 10 times the alphabet, in lowercase, followed by a new line
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase, followed by a new line
  */
 void print_alphabet_x10(void)
 {
-  int i, j;
-  
-  for(i = 0 ; i < 10 ; i++)
-    {
-      for(j = 'a' ; j <= 'z' ; j++)
+	int i, j;
+
+	for (i = 0; i < 10; i++)
 	{
-	  _putchar(j);
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+
+		_putchar('\n');
 	}
-      
-      _putchar('\n');
-    }
 }
