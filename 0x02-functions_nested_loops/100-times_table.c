2f8a65b8fda20528c435aaf0f70b41135deb312e
ayoubidb <ayoubidb@gmail.com>
2023-06-21 15:04:02 -0700

    My first commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 		return;
 
-	int i, j;
+	int i, j, product;
 
 	for (i = 0; i <= n; i++)
 	{
@@ -19,7 +19,7 @@ void print_times_table(int n)
 			_putchar(',');
 			_putchar(' ');
 
-			int product = i * j;
+			product = i * j;
 
 			if (product < 10)
 			{
