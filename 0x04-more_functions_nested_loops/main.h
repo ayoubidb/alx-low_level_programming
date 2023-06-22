7335d668c500e2b7192d6295c812105a2cd8be69
ayoubidb <ayoubidb@gmail.com>
2023-06-22 08:27:39 -0700

    My first commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- */
-
-int _isupper(int c);
-
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
 #endif
