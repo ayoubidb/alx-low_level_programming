3b148d2079f9ee8108ea26b661c0c92095dbc275
ayoubidb <ayoubidb@gmail.com>
2023-06-22 04:34:14 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+    int i;
+
+    for (i = 'A'; i <= 'Z'; i++)
+    {
+        if (c == i)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ */
+
+int _isupper(int c);
+
+#endif
