7c805b46af9da956b2d269a7ee988d94f8d7611f
ayoubidb <ayoubidb@gmail.com>
2023-06-26 15:27:26 -0700

    My first commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,19 @@
 /**
  * _strlen - Returns the length of a string.
- * @s: The string to measure.
+ * @s: The string to get the length of.
  *
- * Return: The length of the string.
+ * Return: The length of @s.
  */
 
 int _strlen(char *s)
 {
-        int length = 0;
-        int i;
+	int length = 0;
 
-        while (*s != '\0')
+	while (*s != '\0')
 	{
 		length++;
 		s++;
 	}
 
-        return (length);
+	return (length);
 }
