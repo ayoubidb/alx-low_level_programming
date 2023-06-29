56cede658723de71d7717bc98e0b3045f578d42d
ayoubidb <ayoubidb@gmail.com>
2023-06-29 09:28:57 -0700

    My first commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,38 +15,4 @@ for (i = 0; (i < (n - 1) / 2); i++)
 	a[i] = a[n - 1 - i];
 	a[n - 1 - i] = c;
 	}
-}#include "main.h"
-/**
- * reverse_array - reverse array
- * @a:array
- * @n:integer
- * Return:void
- */
-void reverse_array(int *a, int n)
-{
-int i, c;
-
-for (i = 0; (i < (n - 1) / 2); i++)
-	{
-	c = a[i];
-	a[i] = a[n - 1 - i];
-	a[n - 1 - i] = c;
-	}
-}#include "main.h"
-/**
- * reverse_array - reverse array
- * @a:array
- * @n:integer
- * Return:void
- */
-void reverse_array(int *a, int n)
-{
-int i, c;
-
-for (i = 0; (i < (n - 1) / 2); i++)
-	{
-	c = a[i];
-	a[i] = a[n - 1 - i];
-	a[n - 1 - i] = c;
-	}
 }
