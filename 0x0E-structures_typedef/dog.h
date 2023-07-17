3f58225350f403bd650df2bea992816af5314994
ayoubidb <ayoubidb@gmail.com>
2023-07-17 14:44:58 -0700

    My first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - basic structure of a dog
+ * @name: name of the dog.
+ * @age: how old the dog is.
+ * @owner: the owner of the dog.
+ *
+ * Description: User-defined data type for a dog.
+ */
 struct dog
 {
 	char *name;
