b1e7f75dcc99ac43831aeb14bfe1732ed811e551
ayoubidb <ayoubidb@gmail.com>
2023-07-19 06:46:45 -0700

    My first commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
-	while(i < size)
-	{
-		action(array[i]);
-		i++;
-	}
+	if(array && action)
+		while(i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 }
