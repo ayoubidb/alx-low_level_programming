743346e35ddedd09f7a3e76ea375d06a8d8fef31
ayoubidb <ayoubidb@gmail.com>
2023-12-07 13:40:48 -0800

    done

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,16 +9,16 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	int counter = 0;
+  int counter = 0;
 
-	if (h == NULL)
-		return (counter);
+  if (h == NULL)
+    return (counter);
 
-	while (h != NULL)
-	{
-		counter++;
-		h = h->next;
-	}
+  while (h != NULL)
+    {
+      counter++;
+      h = h->next;
+    }
 
-	return (counter);
+  return (counter);
 }
