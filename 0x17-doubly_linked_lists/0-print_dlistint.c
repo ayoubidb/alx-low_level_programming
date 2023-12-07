fe57b725be7e0d9d1b113e17119d7ee701255102
ayoubidb <ayoubidb@gmail.com>
2023-12-07 13:55:52 -0800

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,15 @@
  **/
 size_t print_dlistint(const dlistint_t *h)
 {
-  const dlistint_t *node = h;
-  size_t cont = 0;
-  
-  while (node)
-    {
-      printf("%i\n", node->n);
-      cont++;
-      node = node->next;
-    }
-  
-  return (cont);
+	const dlistint_t *node = h;
+	size_t cont = 0;
+
+	while (node)
+	{
+		printf("%i\n", node->n);
+		cont++;
+		node = node->next;
+	}
+
+	return (cont);
 }
