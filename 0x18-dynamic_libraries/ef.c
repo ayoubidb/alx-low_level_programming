0fa4a1cae84e76f07adf9e2dee3046512334f66c
ayoubidb <ayoubidb@gmail.com>
2023-12-11 15:52:30 -0800

    My first commit

---
+++ 0x18-dynamic_libraries/ef.c
@@ -0,0 +1,83 @@
+// empty_functions.c
+
+#include "main.h"
+
+int _putchar(char c) {
+    return 0; // Empty implementation
+}
+
+int _islower(int c) {
+    return 0; // Empty implementation
+}
+
+int _isalpha(int c) {
+    return 0; // Empty implementation
+}
+
+int _abs(int n) {
+    return 0; // Empty implementation
+}
+
+int _isupper(int c) {
+    return 0; // Empty implementation
+}
+
+int _isdigit(int c) {
+    return 0; // Empty implementation
+}
+
+int _strlen(char *s) {
+    return 0; // Empty implementation
+}
+
+void _puts(char *s) {
+    // Empty implementation
+}
+
+char *_strcpy(char *dest, char *src) {
+    return dest; // Empty implementation
+}
+
+int _atoi(char *s) {
+    return 0; // Empty implementation
+}
+
+char *_strcat(char *dest, char *src) {
+    return dest; // Empty implementation
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    return dest; // Empty implementation
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    return dest; // Empty implementation
+}
+
+int _strcmp(char *s1, char *s2) {
+    return 0; // Empty implementation
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    return s; // Empty implementation
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    return dest; // Empty implementation
+}
+
+char *_strchr(char *s, char c) {
+    return NULL; // Empty implementation
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    return 0; // Empty implementation
+}
+
+char *_strpbrk(char *s, char *accept) {
+    return NULL; // Empty implementation
+}
+
+char *_strstr(char *haystack, char *needle) {
+    return NULL; // Empty implementation
+}
